d87f12c19c694f38a0006c632c728cafe398d9c9
Iscava <53660044+Iscava@users.noreply.github.com>
2023-08-16 22:02:06 -0300

    working

--- server.py
+++ server.py
@@ -5,9 +5,27 @@ s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
+
+# Serviço para calculadora simples
+conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+
+opcao = Null
+# Loop para interação entre client e server. Servidor irá parar apenas se o client enviar a mensagem "exit".
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  if data == "exit": break       # stop if client stopped
+  else:
+    match data:
+    case "multiplicar":
+        return "ok"
+    case "dividir":
+        return "Ok"
+    case "Adicionar":
+        return "Ok"
+    case "Subtrair":
+        return "Ok"
+    case _:
+        return "Something's wrong with the internet"
+    print(bytes.decode(data))
+    conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
 conn.close()               # close the connection
