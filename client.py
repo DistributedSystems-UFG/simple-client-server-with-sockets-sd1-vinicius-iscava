862e4370a72a987d87b08440a42a6eeff923f29b
Iscava <53660044+Iscava@users.noreply.github.com>
2023-08-20 17:09:30 -0300

    versão final

--- client.py
+++ client.py
@@ -4,8 +4,9 @@ from constCS import * #-
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
 initMsg = s.recv(1024)     # receive the response
-print (bytes.decode(initMsg))            # print the result
+print (bytes.decode(initMsg))
 
+# Loop da interação
 while True:
   operacao = input("\n\nDigite a operação: ")
   s.send(str.encode(operacao))  # send some data
