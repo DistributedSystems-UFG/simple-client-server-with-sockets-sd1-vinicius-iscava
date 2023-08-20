486916b6cc060413635ac72ec193fff4d87c4be4
Iscava <53660044+Iscava@users.noreply.github.com>
2023-08-20 15:17:05 -0300

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,14 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
+
+while True:
+  operacao = input("\n\nDigite a operação: ")
+  s.send(str.encode(operacao))  # send some data
+  data = s.recv(1024)     # receive the response
+  if data=="exit":
+    print ("Encerrando...")
+    break
+  else:
+    print (bytes.decode(data))            # print the result
 s.close()               # close the connection
