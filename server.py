c550a9b08dce40382bf1b5c5802110c5586b39cb
Iscava <53660044+Iscava@users.noreply.github.com>
2023-08-20 14:50:55 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,25 +7,66 @@ s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 
 # Serviço para calculadora simples
-conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+conn.send(str.encode(bytes.decode("Calculadora basica\n\nUtilize os operadores \"+\", \"-\", \"*\", \"/\" para adicionar, subtrair, multiplicar e dividir respectivamente.\n\nDigite \"exit\" para encerrar.")))
 
-opcao = Null
 # Loop para interação entre client e server. Servidor irá parar apenas se o client enviar a mensagem "exit".
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
   if data == "exit": break       # stop if client stopped
   else:
-    match data:
-    case "multiplicar":
-        return "ok"
-    case "dividir":
-        return "Ok"
-    case "Adicionar":
-        return "Ok"
-    case "Subtrair":
-        return "Ok"
-    case _:
-        return "Something's wrong with the internet"
-    print(bytes.decode(data))
-    conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+    if (data.find("+">=0)):
+      numeros = data.split('+')
+    else if (data.find("-">=0)):
+      numeros = data.split('-')
+    else if (data.find("*">=0)):
+      numeros = data.split('*')
+    else if (data.find("/">=0)):
+      numeros = data.split('/')
+    else
+      print(bytes.decode("\nEntrada invalida."))
+      conn.send(str.encode(bytes.decode("\nEntrada inválida. Tente novamente!")))
+      continue
+    
+    primeiro_numero = numeros[0].strip()
+    segundo_numero = numeros[1].strip()
+    try:
+        primeiro_numero = int(primeiro_numero)
+    except ValueError:
+        try:
+            primeiro_numero = float(primeiro_numero)
+        except ValueError:
+            try:
+                primeiro_numero = float(primeiro_numero.replace(",", "."))
+            except ValueError:
+                print(bytes.decode("\nEntrada invalida."))
+                conn.send(str.encode(bytes.decode("\nEntrada inválida. Tenten novamente!")))
+                continue
+      
+    try:
+        segundo_numero = int(segundo_numero)
+    except ValueError:
+        try:
+            segundo_numero = float(segundo_numero)
+        except ValueError:
+            try:
+                segundo_numero = float(segundo_numero.replace(",", "."))
+            except ValueError:
+                print(bytes.decode("\nEntrada invalida."))
+                conn.send(str.encode(bytes.decode("\nEntrada invalida. Tente novamente!")))
+                continue
+              
+    print(bytes.decode("Entrada valida. Processando..."))
+    if (data.find("+">=0)):
+      print(bytes.decode("\n" + primeiro_numero + " + " + segundo_numero + " = " + (primeiro_numero+segundo_numero)))
+      conn.send(str.encode(bytes.decode("\n" + primeiro_numero + " + " + segundo_numero + " = " + (primeiro_numero+segundo_numero)))) 
+    else if (data.find("-">=0)):
+      print(bytes.decode("\n" + primeiro_numero + " - " + segundo_numero + " = " + (primeiro_numero-segundo_numero)))
+      conn.send(str.encode(bytes.decode("\n" + primeiro_numero + " - " + segundo_numero + " = " + (primeiro_numero-segundo_numero)))) 
+    else if (data.find("*">=0)):
+      print(bytes.decode("\n" + primeiro_numero + " * " + segundo_numero + " = " + (primeiro_numero*segundo_numero)))
+      conn.send(str.encode(bytes.decode("\n" + primeiro_numero + " * " + segundo_numero + " = " + (primeiro_numero*segundo_numero)))) 
+    else if (data.find("/">=0)):
+      print(bytes.decode("\n" + primeiro_numero + " / " + segundo_numero + " = " + (primeiro_numero/segundo_numero)))
+      conn.send(str.encode(bytes.decode("\n" + primeiro_numero + " / " + segundo_numero + " = " + (primeiro_numero/segundo_numero)))) 
+
 conn.close()               # close the connection
